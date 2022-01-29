f1c1ef28089b43ac3b40acd20418dd17be658ecf
krishgon <krishgondia@gmail.com>
2022-01-29 10:59:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) != Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+            console.log(this.hash);
+        }
+
+        console.log("Block mined: " + this.hash);
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
+    }
+
+    createGenesisBlock(){
+        var genBlock =  new Block(0, "01/01/2022", "Genesis Block", "0");
+        genBlock.hash = genBlock.calculateHash();
+        return genBlock;
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i=1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if (currentBlock.hash != currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash != previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
+
+let krishCoin = new Blockchain();
+console.log("Mining block...");
+krishCoin.addBlock(new Block(1, "28/01/2022", {owner: "krish agrawal"}));
+
